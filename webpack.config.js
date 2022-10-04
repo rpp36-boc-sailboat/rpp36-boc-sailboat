498b37fa1f100dd1c0140615091f71e2ff37647b
Othniel Chan <othnielhr.chan@gmail.com>
2022-10-03 21:12:29 -0500

    (init)

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/public');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  // mode: 'development',
+  entry: `${SRC_DIR}/App.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: {
+          loader: 'url-loader?limit=100000',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CompressionPlugin(
+      {
+        deleteOriginalAssets: true,
+      }
+
+    ),
+  ],
+};
