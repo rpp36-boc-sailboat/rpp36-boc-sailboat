214a710016ada563ae02b4afddbf937a2f77bc4b
Othniel Chan <othnielhr.chan@gmail.com>
2022-10-03 21:16:45 -0500

    (init)

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true});
+app.use(express.urlencoded({extended: true}));
 
 app.listen(port, () => {
   console.log('listening on port: ', port);
