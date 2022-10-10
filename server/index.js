d668423aa4fce47c12a5e181fd423d4311386654
Filimon Kiros <filimona.kiros@gmail.com>
2022-10-10 15:33:04 -0400

    additional repo setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log('listening on port: ', port);
+app.use(express.static("client/public"));
+
+app.get("/test", (req, res) => {
+  // res.send("Greetings!");
+  res.status(200);
+  res.end();
 });
+
+// app.listen(port, () => {
+// console.log("listening on port: ", port);
+// });
+
+module.exports = app;
