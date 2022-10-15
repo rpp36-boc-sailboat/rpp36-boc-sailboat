7dc175b23b907102f19750add68ca5775216a3c4
AaronW <aaronwang96@hotmail.com>
2022-10-16 03:08:56 +0900

    include App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,23 @@ import SignIn from "./Components/Accounts/SignIn.jsx";
 import SignUp from "./Components/Accounts/SignUp.jsx";
 import Metrics from "./Components/Metrics/index.jsx";
 import Calendar from "./Components/Calendar.jsx";
+// import TodoCreate from './components/TodoCreate.jsx';
+// import CategoryCreate from './components/CategoryCreate.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       test: 1,
+      categories: [
+        {key: 'None', value: 'none'},
+        {key: 'Option 1', value: 'option1'},
+        {key: 'Option 2', value: 'option2'},
+        {key: 'Option 3', value: 'option3'},
+        {key: 'Option 4', value: 'option4'},
+        {key: 'Option 5', value: 'option5'},
+        {key: 'Other', value: 'other'}
+      ]
     };
   }
 
@@ -21,6 +32,10 @@ class App extends React.Component {
         <SignUp />
         <Metrics />
         <Calendar />
+        <h1>THIS CREATES A TODO ENTRY</h1>
+        <TodoCreate categories={this.state.categories}/>
+        <h1>THIS CREATES A CATEGORY</h1>
+        <CategoryCreate />
       </div>
     );
   }
