6d1e7afd895311144dfc653ebee19200ec84dd86
AaronW <aaronwang96@hotmail.com>
2022-10-16 03:12:56 +0900

    include forms

---
+++ client/src/Components/Forms/TodoCreate.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { useFormik, useField } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const TodoCreate = (props) => {
+
+  const formik = useFormik({
+    initialValues: {
+      taskName: '',
+      start: '',
+      end: '',
+      category: 'none',
+      description: '',
+      completed: false,
+      appointment: false
+    },
+    validationSchema: Yup.object({
+      taskName: Yup.string()
+        .required('Required'),
+      category: Yup.string()
+        .required('Required')
+    }),
+    onSubmit: values => {
+       axios.post('/todo', values)
+    },
+  });
+
+  return(
+  <form onSubmit={formik.handleSubmit}>
+    <label htmlFor="taskName">Task Name </label>
+    <input
+    id="taskName"
+    name="taskName"
+    type="text"
+    onChange={formik.handleChange}
+    value={formik.values.taskName}
+    />
+    {formik.touched.taskName && formik.errors.taskName ? (
+    <div>{formik.errors.taskName}</div>
+    ) : null}
+
+    <label htmlFor="start">Start </label>
+    <input
+    id="start"
+    name="start"
+    type="datetime-local"
+    onChange={formik.handleChange}
+    value={formik.values.start}
+    />
+
+    <label htmlFor="end">End </label>
+    <input
+    id="end"
+    name="end"
+    type="datetime-local"
+    onChange={formik.handleChange}
+    value={formik.values.end}
+    />
+
+    <label htmlFor="category">Category </label>
+    <select
+        name="category"
+        value={formik.values.category}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        style={{ display: "block" }}
+      >
+        {props.categories.map(option => {
+          return (
+            <option value={option.value} label={option.key} />
+          )
+        })}
+    </select>
+    <button>Add Category</button>
+
+    <label htmlFor="description">Description </label>
+    <input
+    id="description"
+    name="description"
+    type="text"
+    onChange={formik.handleChange}
+    value={formik.values.description}
+    />
+
+    <button type="submit">Submit</button>
+  </form>)
+}
+
+export default TodoCreate;
