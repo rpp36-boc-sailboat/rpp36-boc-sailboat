0286f5d7478ed0bfde847015db493ff3c4ce29e1
Othniel Chan <othnielhr.chan@gmail.com>
2022-10-17 23:36:40 -0500

    uncommented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,17 +32,17 @@ class App extends React.Component {
 
   componentDidMount() {
     let categories = [];
-    // axios.get('/categories', {
-    //   params: {
-    //     id: this.state.userID
-    //   }
-    // })
-    // .then(result => result.data.map((option, i) => {
-    //   return categories.push({key: option.category, value: option.category_id})
-    // }))
-    // .then(this.setState({
-    //   categories: categories
-    // }))
+    axios.get('/categories', {
+      params: {
+        id: this.state.userID
+      }
+    })
+    .then(result => result.data.map((option, i) => {
+      return categories.push({key: option.category, value: option.category_id})
+    }))
+    .then(this.setState({
+      categories: categories
+    }))
   }
 
   render() {
