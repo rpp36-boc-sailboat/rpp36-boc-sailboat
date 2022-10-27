63772e8502d361a94e088f56449b825e84cc0b79
AaronW <aaronwang96@hotmail.com>
2022-10-27 16:58:03 +0900

    created category modal

--- client/src/Components/Forms/CategoryCreate.jsx
+++ client/src/Components/Forms/CategoryCreate.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { useFormik, useField } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
+import Modal from 'react-modal';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
 
 const CategoryCreate = (props) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const formik = useFormik({
     initialValues: {
@@ -24,27 +30,30 @@ const CategoryCreate = (props) => {
   });
 
   return(
-  <form onSubmit={formik.handleSubmit}>
-    <label htmlFor="category">Category </label>
-    <input
-    id="category"
-    name="category"
-    type="text"
-    onChange={formik.handleChange}
-    value={formik.values.category}
-    />
+  <form onSubmit={formik.handleSubmit} class="category-modal">
+    <div class="modal-content">
+      <label htmlFor="category">Category </label>
+      <input
+      id="category"
+      name="category"
+      type="text"
+      onChange={formik.handleChange}
+      value={formik.values.category}
+      />
 
-    <label htmlFor="color">Color </label>
-    <input
-    id="color"
-    name="color"
-    type="color"
-    onChange={formik.handleChange}
-    value={formik.values.color}
-    />
+      <label htmlFor="color">Color </label>
+      <input
+      id="color"
+      name="color"
+      type="color"
+      onChange={formik.handleChange}
+      value={formik.values.color}
+      />
 
-    <button type="submit">Submit</button>
-  </form>)
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+  )
 }
 
 export default CategoryCreate;
